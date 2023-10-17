0774c1157ee505345f59da54b47bb047f22c62a6
Zain Javaid <zainmfj@gmail.com>
2023-10-17 17:53:00 -0500

    Added first pages to website

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'ZAGAttack! 🤓'
+    return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/schedule')
+def schedule():
+    return 'Schedule'
+
+@app.route('/members')
+def members():
+    return 'Members'
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+    
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
